6d2197d65d2fd497e4cdb6ae46a8f1165f5bb51f
sneha-1019 <sneha.tripathi2015@gmail.com>
2024-07-11 00:00:37 +0530

    fornt-end complete

--- server/index.js
+++ server/index.js
@@ -1,8 +1,33 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const petRouter = require('./Routes/PetRoute')
+const AdoptFormRoute = require('./Routes/AdoptFormRoute')
+const cors = require('cors');
+const path = require('path');
+
 const app = express();
 
+app.use(cors());
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
+app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000)
+app.use(petRouter)
+app.use('/form', AdoptFormRoute)
+// app.use('/admin', AdminRoute)
+
+mongoose.connect('mongodb://localhost:27017')
+    .then(() => {
+        console.log('Connected to DB');
+        const PORT = 4000;
+        app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.error(err);
+    })
